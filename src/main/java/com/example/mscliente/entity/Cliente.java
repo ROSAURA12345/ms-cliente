4da99015cd3cec909cc1a6ae8b451d84dc84d161
David.Yucra-Mamani <103071814+yucradavid@users.noreply.github.com>
2024-04-02 22:25:59 -0500

    nuevo poroyecto

---
+++ src/main/java/com/example/mscliente/entity/Cliente.java
@@ -0,0 +1,27 @@
+package com.example.mscliente.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+
+@Entity
+@Data
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nombre;
+    private String dni;
+    private String domicilio;
+    private Number telefono;
+    private BigDecimal limite;
+    private String fecha;
+    private String compras;
+    private Number abono;
+
+}
